a237aa044fb9374ff305e107294083bbaa081dc6
Alex-Gombos <74148529+Alex-Gombos@users.noreply.github.com>
2023-04-20 00:38:58 +0300

    ValueError: You should supply an encoding or a list of encodings to this method that includes input_ids

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ from transformers import DataCollatorForTokenClassification
 from transformers import AutoModelForTokenClassification
 import json
 import torch
+from sklearn.model_selection import train_test_split
 
 
 tokenizer = BertTokenizerFast.from_pretrained("dumitrescustefan/bert-base-romanian-uncased-v1", do_lower_case=True,model_max_length=512)
+
 
 with open('output.txt', 'r') as f:
     data = json.load(f)
 
 dataset = Dataset.from_list(data)
+
 
 label_names = sorted(set(label for labels in dataset["ner_tags"] for label in labels))
 dataset = dataset.cast_column("ner_tags", Sequence(ClassLabel(names=label_names)))
@@ -22,33 +25,32 @@ dataset = dataset.cast_column("ner_tags", Sequence(ClassLabel(names=label_names)
 
 print(dataset.features["ner_tags"])
 
+train_dataset, val_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
+
+train_datasetDataset = Dataset.from_dict(train_dataset)
+val_datasetDataset = Dataset.from_dict(val_dataset)
+
+print(len(train_datasetDataset))
+print(train_datasetDataset[0])
+
+print(len(val_datasetDataset))
+print(val_datasetDataset[0])
+
+
 def tokenize_and_align_labels(examples, label_all_tokens=True): 
     tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True) 
     labels = [] 
     for i, label in enumerate(examples["ner_ids"]): 
         word_ids = tokenized_inputs.word_ids(batch_index=i) 
-        # word_ids() => Return a list mapping the tokens
-        # to their actual word in the initial sentence.
-        # It Returns a list indicating the word corresponding to each token. 
         previous_word_idx = None 
         label_ids = []
-        # Special tokens like `<s>` and `<\s>` are originally mapped to None 
-        # We need to set the label to -100 so they are automatically ignored in the loss function.
         for word_idx in word_ids: 
             if word_idx is None: 
-                # set –100 as the label for these special tokens
                 label_ids.append(-100)
-            # For the other tokens in a word, we set the label to either the current label or -100, depending on
-            # the label_all_tokens flag.
-            elif word_idx != previous_word_idx:
-                # if current word_idx is != prev then its the most regular case
-                # and add the corresponding token                 
+            elif word_idx != previous_word_idx:               
                 label_ids.append(label[word_idx]) 
             else: 
-                # to take care of sub-words which have the same word_idx
-                # set -100 as well for them, but only if label_all_tokens == False
                 label_ids.append(label[word_idx] if label_all_tokens else -100) 
-                # mask the subword representations after the first subword
                  
             previous_word_idx = word_idx 
         labels.append(label_ids) 
@@ -57,7 +59,7 @@ def tokenize_and_align_labels(examples, label_all_tokens=True):
 
 tokenized_datasets = dataset.map(tokenize_and_align_labels, batched=True)
 
-model = AutoModelForTokenClassification.from_pretrained("dumitrescustefan/bert-base-romanian-uncased-v1", num_labels=81)
+model = AutoModelForTokenClassification.from_pretrained("dumitrescustefan/bert-base-romanian-uncased-v1", num_labels=3)
 
 from transformers import TrainingArguments, Trainer 
 args = TrainingArguments( 
@@ -67,7 +69,8 @@ learning_rate=2e-5,
 per_device_train_batch_size=2, 
 per_device_eval_batch_size=2, 
 num_train_epochs=1, 
-weight_decay=0.01, 
+weight_decay=0.01,
+remove_unused_columns=False
 )
 data_collator = DataCollatorForTokenClassification(tokenizer)
 
@@ -110,8 +113,8 @@ def compute_metrics(eval_preds):
 trainer = Trainer( 
     model, 
     args, 
-   train_dataset=tokenized_datasets, 
-   eval_dataset=tokenized_datasets, 
+   train_dataset=train_datasetDataset, 
+   eval_dataset=val_datasetDataset, 
    data_collator=data_collator, 
    tokenizer=tokenizer, 
    compute_metrics=compute_metrics,
